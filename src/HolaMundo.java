f48c08a22ceaa070191db0bb5a41e02670dec400
alefer94 <afpy1994@gmail.com>
2023-07-14 02:00:11 -0500

    Estructuras Control

---
+++ src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+
+    public static void main(String[] args){
+
+        String saludar = "Hola";
+
+        System.out.println(saludar.toLowerCase());
+    }
+}
---
+++ src/control_structures/Example.java
@@ -0,0 +1,23 @@
+package control_structures;
+
+public class Example {
+
+    public static void main(String[] arg) {
+
+        boolean verdad = true;
+
+        System.out.println(verdad);
+
+        if (verdad) {
+            System.out.println("Es verdad");
+        } else {
+            System.out.println("Es falso");
+        }
+
+        var numero3 = "15";
+
+        System.out.println(numero3);
+
+
+    }
+}
